b12328dad409b203ebcd63a13aac3de1671fb7c7
manohar sai <powermart@gmail.com>
2024-10-03 15:45:52 +0530

    Bhais second commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+///import {store} from './redux/store'
+import Start from './components/start'
+// import {useDispatch,useSelector} from 'react-redux'
+// import {addFunction,subFunction} from './action'
+
+
+function App() {
+ 
+  return (
+    <div>
+    <Start/>
+    </div>
+  )
+}
+
+export default App
